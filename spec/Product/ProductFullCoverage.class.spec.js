1b8322de09539e9eca3a9aefc8d98a034f31d212
Iván Hernández Durán <ivan.hernandez@tcpip.tech>
2021-01-31 01:37:43 -0600

    Update names for `ProductFullCoverage` unit tests.

--- spec/Product/ProductFullCoverage.class.spec.js
+++ spec/Product/ProductFullCoverage.class.spec.js
@@ -5,7 +5,7 @@ const { PRODUCT_COVERAGE } = require('../../src/enums/ProductCoverage.enum');
 
 describe(`Testing '${ProductFullCoverage.name}' class`, () => {
 	it('Should update product price where it `sellIn` and `price` values is greater than 1', () => {
-		const product = new ProductFullCoverage(PRODUCT_COVERAGE.MEGA_COVERAGE, 5, 10);
+		const product = new ProductFullCoverage(PRODUCT_COVERAGE.FULL_COVERAGE, 5, 10);
 		product.updatePrice();
 
 		expect(product.sellIn).equal(4);
@@ -13,7 +13,7 @@ describe(`Testing '${ProductFullCoverage.name}' class`, () => {
 	});
 
 	it('Should update product price and try to modify it `maxPrice` value', () => {
-		const product = new ProductFullCoverage(PRODUCT_COVERAGE.MEGA_COVERAGE, 5, 10);
+		const product = new ProductFullCoverage(PRODUCT_COVERAGE.FULL_COVERAGE, 5, 10);
 		product.maxPrice = 80;
 		product.updatePrice();
 		const maxPrice = product.maxPrice;
@@ -24,7 +24,7 @@ describe(`Testing '${ProductFullCoverage.name}' class`, () => {
 	});
 
 	it('Should update product price where it `sellIn` value is 0', () => {
-		const product = new ProductFullCoverage(PRODUCT_COVERAGE.MEGA_COVERAGE, 0, 10);
+		const product = new ProductFullCoverage(PRODUCT_COVERAGE.FULL_COVERAGE, 0, 10);
 		product.updatePrice();
 
 		expect(product.sellIn).equal(-1);
@@ -32,7 +32,7 @@ describe(`Testing '${ProductFullCoverage.name}' class`, () => {
 	});
 
 	it('Should update product price where it `sellIn` value is -5', () => {
-		const product = new ProductFullCoverage(PRODUCT_COVERAGE.MEGA_COVERAGE, -5, 10);
+		const product = new ProductFullCoverage(PRODUCT_COVERAGE.FULL_COVERAGE, -5, 10);
 		product.updatePrice();
 
 		expect(product.sellIn).equal(-6);
@@ -40,7 +40,7 @@ describe(`Testing '${ProductFullCoverage.name}' class`, () => {
 	});
 
 	it('Should update product price where it `price` value is 0', () => {
-		const product = new ProductFullCoverage(PRODUCT_COVERAGE.MEGA_COVERAGE, 5, 0);
+		const product = new ProductFullCoverage(PRODUCT_COVERAGE.FULL_COVERAGE, 5, 0);
 		product.updatePrice();
 
 		expect(product.sellIn).equal(4);
@@ -48,7 +48,7 @@ describe(`Testing '${ProductFullCoverage.name}' class`, () => {
 	});
 
 	it('Should update product price where it `price` value is 50', () => {
-		const product = new ProductFullCoverage(PRODUCT_COVERAGE.MEGA_COVERAGE, 5, 50);
+		const product = new ProductFullCoverage(PRODUCT_COVERAGE.FULL_COVERAGE, 5, 50);
 		product.updatePrice();
 
 		expect(product.sellIn).equal(4);
